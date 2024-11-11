0bbbcdfa7d2206e239988a5aba0823ae0dc09c8d
Hans-Wurst-21 <github+mempool-space@hansmail.net>
2024-11-11 23:26:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+<!--
+    strg+k dann v
+    Öffnet live Darstellung
+-->
+
+![Logo](admin/mempool-space.png)
+
+# ioBroker.mempool-space
+
+[![NPM version](https://img.shields.io/npm/v/iobroker.mempool-space.svg)](https://www.npmjs.com/package/iobroker.mempool-space)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.mempool-space.svg)](https://www.npmjs.com/package/iobroker.mempool-space)
+![Number of Installations](https://iobroker.live/badges/mempool-space-installed.svg)
+![Current version in stable repository](https://iobroker.live/badges/mempool-space-stable.svg)
+
+[![NPM](https://nodei.co/npm/iobroker.mempool-space.png?downloads=true)](https://nodei.co/npm/iobroker.mempool-space/)
+
+**Tests:** ![Test and Release](https://github.com/Hans-Wurst-21/ioBroker.mempool-space/workflows/Test%20and%20Release/badge.svg)
+
+## mempool.space adapter for ioBroker
+
+Live data from mempool.space websocket.
+
+Connect the bitcoin Network to your smarthome.
+
+## Live data websocket
+
+This section is intended for the developer. It can be deleted later.
+
+### DISCLAIMER
+
+Please make sure that you consider copyrights and trademarks when you use names or logos of a company and add a disclaimer to your README.
+You can check other adapters for examples or ask in the developer community. Using a name or logo of a company without permission may cause legal problems for you.
+
+## Changelog
+
+<!--
+    Placeholder for the next version (at the beginning of the line):
+    ### **WORK IN PROGRESS**
+-->
+
+### **WORK IN PROGRESS**
+
+-   (mempool-space) initial release
+
+## License
+
+MIT License
+
+Copyright (c) 2024 Hans-Wurst-21 <mail@hansmail.net>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,239 @@
+'use strict';
+
+const utils = require('@iobroker/adapter-core');
+const mempoolJS = require('@mempool/mempool.js');
+const { DateTime } = require('luxon');
+
+class mempoolspace extends utils.Adapter {
+    constructor(options) {
+        super({
+            ...options,
+            name: 'mempool-space',
+        });
+        this.on('ready', this.onReady.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+        this.ws = null;
+        this.updateInterval = null;
+    }
+
+    async onReady() {
+        this.log.info('mempool.space adapter is ready');
+
+        const websocketUrl = this.config.websocketUrl || 'wss://mempool.space/api/v1/ws';
+
+        this.initWebSocket(websocketUrl);
+        this.updateInterval = setInterval(() => {
+            this.updateTimeSinceLastBlock();
+            this.updateRemainingTimes();
+        }, 60000); // Aktualisiere jede Minute
+    }
+
+    initWebSocket(url) {
+        this.log.info(`Initializing WebSocket connection to ${url}`);
+
+        const {
+            bitcoin: { websocket },
+        } = mempoolJS({
+            hostname: new URL(url).hostname,
+        });
+
+        this.ws = websocket.initServer({
+            options: ['blocks', 'stats', 'mempool-blocks', 'live-2h-chart'],
+        });
+
+        this.ws.on('message', this.handleWebSocketMessage.bind(this));
+        this.ws.on('error', this.handleWebSocketError.bind(this));
+        this.ws.on('close', this.handleWebSocketClose.bind(this));
+        this.ws.on('open', this.handleWebSocketOpen.bind(this));
+
+        this.setStateAsync('status.websocketConnection', { val: false, ack: true });
+    }
+
+    handleWebSocketMessage(data) {
+        try {
+            const res = JSON.parse(data.toString());
+            this.processConversionData(res);
+            this.processFeeData(res);
+            this.processBlockData(res);
+            this.processNetworkData(res);
+            this.processMempoolData(res);
+        } catch (error) {
+            this.log.error(`Error processing WebSocket message: ${error}`);
+        }
+    }
+
+    processConversionData(data) {
+        if (data.conversions) {
+            this.setStateAsync('conversion.usd', { val: data.conversions.USD, ack: true });
+            this.setStateAsync('conversion.eur', { val: data.conversions.EUR, ack: true });
+            this.setStateAsync('conversion.timestamp', { val: data.conversions.time, ack: true });
+
+            // Berechnung der Moscow Time
+            const satoshisPerBTC = 100000000; // 1 BTC = 100,000,000 Satoshis
+            const moscowTimeUSD = Math.floor(satoshisPerBTC / data.conversions.USD);
+            const moscowTimeEUR = Math.floor(satoshisPerBTC / data.conversions.EUR);
+
+            // Formatierung der Moscow Time
+            const formatMoscowTime = (value) => {
+                const strValue = value.toString().padStart(4, '0');
+                return strValue.slice(0, 2) + ':' + strValue.slice(2);
+            };
+
+            const formattedMoscowTimeUSD = formatMoscowTime(moscowTimeUSD);
+            const formattedMoscowTimeEUR = formatMoscowTime(moscowTimeEUR);
+
+            this.setStateAsync('conversion.moscowtimeUSD', { val: formattedMoscowTimeUSD, ack: true });
+            this.setStateAsync('conversion.moscowtimeEUR', { val: formattedMoscowTimeEUR, ack: true });
+        }
+    }
+
+    processFeeData(data) {
+        if (data.fees) {
+            this.setStateAsync('fees.fastest', { val: data.fees.fastestFee, ack: true });
+            this.setStateAsync('fees.halfHour', { val: data.fees.halfHourFee, ack: true });
+            this.setStateAsync('fees.hour', { val: data.fees.hourFee, ack: true });
+            this.setStateAsync('fees.economy', { val: data.fees.economyFee, ack: true });
+            this.setStateAsync('fees.minimum', { val: data.fees.minimumFee, ack: true });
+        }
+    }
+
+    processBlockData(data) {
+        if (data.block) {
+            this.setStateAsync('block.height', { val: Number(data.block.height), ack: true });
+            this.setStateAsync('block.hash', { val: data.block.id, ack: true });
+            this.setStateAsync('block.timestamp', { val: data.block.timestamp, ack: true });
+            this.updateTimeSinceLastBlock();
+
+            if (data.block.extras && data.block.extras.pool) {
+                this.setStateAsync('block.miningPool', { val: data.block.extras.pool.name, ack: true });
+            }
+        }
+    }
+
+    processNetworkData(data) {
+        if (data.da) {
+            // Durchschnittliche Blockzeit
+            if (data.da.timeAvg) {
+                const avgBlockTimeMinutes = data.da.timeAvg / 60; // Umrechnung von Sekunden in Minuten
+                const roundedMinutes = Math.round(avgBlockTimeMinutes / 100) / 10; // Runden auf eine Dezimalstelle
+                this.setStateAsync('network.averageBlockTime', { val: roundedMinutes, ack: true });
+            }
+
+            // Schwierigkeitsanpassung in Prozent
+            if (data.da.difficultyChange) {
+                const roundedDifficultyChange = Math.round(data.da.difficultyChange * 100) / 100;
+                this.setStateAsync('network.difficultyChange', { val: roundedDifficultyChange, ack: true });
+            }
+
+            // Letzte Schwierigkeitsanpassung in Prozent
+            if (data.da.previousRetarget) {
+                const roundedPreviousDifficultyChange = Math.round(data.da.previousRetarget * 100) / 100;
+                this.setStateAsync('network.previousDifficultyChange', {
+                    val: roundedPreviousDifficultyChange,
+                    ack: true,
+                });
+            }
+
+            // Nächste Schwierigkeitsanpassung
+            if (data.da.estimatedRetargetDate) {
+                this.setStateAsync('network.nextDifficultyAdjustment', {
+                    val: data.da.estimatedRetargetDate,
+                    ack: true,
+                });
+            }
+        }
+
+        this.updateRemainingTimes();
+    }
+
+    processMempoolData(data) {
+        if (data.mempoolInfo) {
+            this.setStateAsync('mempool.transactionCount', { val: data.mempoolInfo.size, ack: true });
+        }
+    }
+
+    updateTimeSinceLastBlock() {
+        this.getStateAsync('block.timestamp').then((state) => {
+            if (state && state.val) {
+                const lastBlockTimestamp = state.val;
+                const currentTime = Math.floor(Date.now() / 1000);
+                const elapsedTime = currentTime - Number(lastBlockTimestamp);
+                const minutesPassed = Math.floor(elapsedTime / 60);
+                const timeSinceLastBlock = `vor ${minutesPassed} Minuten`;
+                this.setStateAsync('block.timeSinceLastBlock', { val: timeSinceLastBlock, ack: true });
+            }
+        });
+    }
+
+    updateRemainingTimes() {
+        this.getStateAsync('network.nextDifficultyAdjustment').then((state) => {
+            if (state && state.val) {
+                const nextDifficultyAdjustment = DateTime.fromMillis(state.val);
+                const now = DateTime.now();
+                const remainingDays = nextDifficultyAdjustment.diff(now, 'days').days;
+                const formattedDays = remainingDays.toFixed(1);
+                this.setStateAsync('network.remainingTimeToDifficulty', {
+                    val: `in ${formattedDays} Tagen`,
+                    ack: true,
+                });
+            }
+        });
+
+        this.getStateAsync('block.height').then((state) => {
+            if (state && typeof state.val === 'number') {
+                const currentBlockHeight = state.val;
+                const nextHalvingBlock = Math.ceil(currentBlockHeight / 210000) * 210000;
+                const blocksUntilHalving = nextHalvingBlock - currentBlockHeight;
+                const minutesUntilHalving = blocksUntilHalving * 10; // Annahme: 10 Minuten pro Block im Durchschnitt
+
+                const now = DateTime.now();
+                const halvingDate = now.plus({ minutes: minutesUntilHalving });
+
+                const duration = halvingDate.diff(now, ['years', 'days']);
+                const years = Math.floor(duration.years);
+                const days = Math.floor(duration.days);
+
+                this.setStateAsync('network.remainingTimeToHalving', {
+                    val: `in ${years} Jahren, ${days} Tagen`,
+                    ack: true,
+                });
+            }
+        });
+    }
+
+    handleWebSocketError(error) {
+        this.log.error(`WebSocket error: ${error}`);
+        this.setStateAsync('status.websocketConnection', { val: false, ack: true });
+    }
+
+    handleWebSocketClose() {
+        this.log.info('WebSocket connection closed');
+        this.setStateAsync('status.websocketConnection', { val: false, ack: true });
+    }
+
+    handleWebSocketOpen() {
+        this.log.info('WebSocket connection opened');
+        this.setStateAsync('status.websocketConnection', { val: true, ack: true });
+    }
+
+    onUnload(callback) {
+        try {
+            if (this.ws) {
+                this.ws.close();
+                this.log.info('WebSocket connection closed');
+            }
+            if (this.updateInterval) {
+                clearInterval(this.updateInterval);
+            }
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+}
+
+if (require.main !== module) {
+    module.exports = (options) => new mempoolspace(options);
+} else {
+    new mempoolspace();
+}
